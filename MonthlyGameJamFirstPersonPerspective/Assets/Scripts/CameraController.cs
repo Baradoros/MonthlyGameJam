b328c7303409f9acd7d49b9f04a8e4f2de7d88f7
Baradoros <thatfunderburkguy@gmail.com>
2019-04-03 21:49:04 -0500

    Basic Testing scene and player movement

---
+++ MonthlyGameJamFirstPersonPerspective/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+    public float sensitivity = 5.0f;
+
+	void Start () {
+		player = transform.parent.gameObject;
+	}
+	
+
+	void FixedUpdate () {
+		float y = Input.GetAxisRaw("Mouse X");
+		float x = -Input.GetAxisRaw("Mouse Y");
+		player.transform.Rotate(0, y, 0);
+		transform.Rotate(x, 0, 0);
+	}
+		
+}
---
+++ MonthlyGameJamFirstPersonPerspective/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 4f;
+    public float sprintSpeedMultiplier = 2f;
+	public float jumpVelocity = 5f;
+    [Space]
+	public Transform floorDetector;
+
+	private Rigidbody rb;
+	private HeadBobber hb;
+
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody>();
+		hb = GetComponentInChildren<HeadBobber>();
+	}
+
+	void Update() {
+		if (IsGrounded())
+			hb.enabled = true;
+		else
+			hb.enabled = false;
+	}
+
+	void FixedUpdate() {
+        HandleMovement();
+
+        // Hardcoding just for dev purposes calm down
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    private void HandleMovement() {
+        float forward;
+        float strafe;
+
+        if (Input.GetButton("Sprint")) {
+            hb.speedMult = sprintSpeedMultiplier;
+            forward = Input.GetAxisRaw("Horizontal") * speed * sprintSpeedMultiplier;
+            strafe = Input.GetAxisRaw("Vertical") * speed * sprintSpeedMultiplier;
+        }
+        else {
+            hb.speedMult = 1;
+            forward = Input.GetAxisRaw("Horizontal") * speed;
+            strafe = Input.GetAxisRaw("Vertical") * speed;
+        }
+
+        Vector3 move = new Vector3(forward, rb.velocity.y, strafe);
+        rb.velocity = transform.TransformDirection(move);
+
+        if (Input.GetAxisRaw("Jump") > 0) {
+            if (IsGrounded()) {
+                rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
+            }
+
+        }
+    }
+
+    // Raycast down to see if player is standing on a collider
+    public bool IsGrounded() {
+		if (Physics.Raycast(floorDetector.position, Vector3.down, 0.05f))
+			return true;
+		else
+			return false;
+	}
+
+}
