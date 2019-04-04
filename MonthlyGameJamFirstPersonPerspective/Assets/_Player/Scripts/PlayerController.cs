0e8e928185814039b14d61bea297c00c27432b96
Baradoros <thatfunderburkguy@gmail.com>
2019-04-04 15:19:10 -0500

    Added line in scene view to represent player pickup distance and direction

--- MonthlyGameJamFirstPersonPerspective/Assets/_Player/Scripts/PlayerController.cs
+++ MonthlyGameJamFirstPersonPerspective/Assets/_Player/Scripts/PlayerController.cs
@@ -2,49 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 4f;
+    public float speed = 4f;
     public float sprintSpeedMultiplier = 2f;
-	public float jumpVelocity = 5f;
+    public float jumpVelocity = 5f;
     [Tooltip("Player compares objects to this LayerMask to determine what can be picked up")]
     public LayerMask whatCanBePickedUp;
     public float pickupDistance;
 
     [Space]
     [Header("References")]
-	public Transform floorDetector;
+    public Transform floorDetector;
     public Transform itemHolder;
+    public Camera camera;
 
-    private Camera camera;
-	private Rigidbody rb;
-	private HeadBobber hb;
+    private Rigidbody rb;
+    private HeadBobber hb;
     private GameObject heldItem;
 
-	void Awake() {
-		Cursor.lockState = CursorLockMode.Locked;
-		rb = GetComponent<Rigidbody>();
-		hb = GetComponentInChildren<HeadBobber>();
+    void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        hb = GetComponentInChildren<HeadBobber>();
         camera = GetComponentInChildren<Camera>();
-	}
+    }
 
-	void Update() {
-		if (IsGrounded())
-			hb.enabled = true;
-		else
-			hb.enabled = false;
+    void Update() {
+        if (IsGrounded())
+            hb.enabled = true;
+        else
+            hb.enabled = false;
 
         if (Input.GetButtonDown("Fire1")) {
             if (heldItem != null) {
                 PickUpObject();
-            } else {
+            }
+            else {
                 DropHeldObject();
             }
         }
-	}
+    }
 
-	void FixedUpdate() {
+    void FixedUpdate() {
         HandleMovement();
         HandleLook();
 
@@ -94,26 +96,30 @@ public class PlayerController : MonoBehaviour {
         // Check to see if we're aiming at something that is in range and can be picked up
         // If so, pick it up
         if (Physics.Raycast(ray, out hit, pickupDistance, whatCanBePickedUp)) {
-            heldItem = hit.collider.gameObject;
-            heldItem.transform.SetParent(transform);
-            heldItem.transform.position = itemHolder.transform.position;
+            if (heldItem == null) {
+                heldItem = hit.collider.gameObject;
+                heldItem.transform.SetParent(transform);
+                heldItem.transform.position = itemHolder.transform.position;
+            }
 
         }
-        
+
     }
 
     private void DropHeldObject() {
-        heldItem.transform.parent = null;
-        heldItem = null;
+        if (heldItem != null) {
+            heldItem.transform.parent = null;
+            heldItem = null;
+        }
     }
 
     // Raycast down to see if player is standing on a collider
     public bool IsGrounded() {
-		if (Physics.Raycast(floorDetector.position, Vector3.down, 0.05f))
-			return true;
-		else
-			return false;
-	}
+        if (Physics.Raycast(floorDetector.position, Vector3.down, 0.05f))
+            return true;
+        else
+            return false;
+    }
 
 
     // Don't ask me how this works it just keeps the camera from rotationg 360 degrees on the x-axis
@@ -130,6 +136,24 @@ public class PlayerController : MonoBehaviour {
         q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
 
         return q;
+    }
+
+}
+
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+    }
+
+    private void OnSceneGUI() {
+        PlayerController player = (PlayerController)target;
+
+        // Draw a line in scene view to represent pickup distance and direction
+        Handles.color = Color.red;
+        Handles.DrawLine(player.camera.transform.position, player.camera.transform.position + new Vector3(0, 0, player.pickupDistance));
     }
 
 }
