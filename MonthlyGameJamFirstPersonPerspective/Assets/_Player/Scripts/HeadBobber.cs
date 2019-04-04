409c53f94665578731d0d7ceab59c5dc5df91922
Baradoros <thatfunderburkguy@gmail.com>
2019-04-03 23:27:42 -0500

    Fixed camera rotating infinitely on the x-axis

---
+++ MonthlyGameJamFirstPersonPerspective/Assets/_Player/Scripts/HeadBobber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBobber : MonoBehaviour {
+
+	private float timer = 0.0f;
+	public bool headBob = true;
+	public float bobbingSpeed = 0.18f;
+	public float bobbingAmount = 0.2f;
+	public float midpoint = 2.0f;
+	[HideInInspector]
+	public float speedMult = 1;
+
+	void Update() {
+		if (!headBob)
+			return;
+		
+		float waveslice = 0.0f;
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 cSharpConversion = transform.localPosition; 
+
+		if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) {
+			timer = 0.0f;
+		} else {
+			waveslice = Mathf.Sin(timer);
+			timer = timer + bobbingSpeed * speedMult;
+			if (timer > Mathf.PI * 2) {
+				timer = timer - (Mathf.PI * 2);
+			}
+		}
+		if (waveslice != 0) {
+			float translateChange = waveslice * bobbingAmount;
+			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+			totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+			translateChange = totalAxes * translateChange;
+			cSharpConversion.y = midpoint + translateChange;
+		} else {
+			cSharpConversion.y = midpoint;
+		}
+
+		transform.localPosition = cSharpConversion;
+	}
+}
---
+++ MonthlyGameJamFirstPersonPerspective/Assets/_Player/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 4f;
+    public float sprintSpeedMultiplier = 2f;
+	public float jumpVelocity = 5f;
+    [Space]
+    public Camera camera;
+	public Transform floorDetector;
+
+	private Rigidbody rb;
+	private HeadBobber hb;
+
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody>();
+		hb = GetComponentInChildren<HeadBobber>();
+	}
+
+	void Update() {
+		if (IsGrounded())
+			hb.enabled = true;
+		else
+			hb.enabled = false;
+	}
+
+	void FixedUpdate() {
+        HandleMovement();
+        HandleLook();
+
+        // Hardcoding just for dev purposes calm down
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    private void HandleLook() {
+        float y = Input.GetAxisRaw("Mouse X");
+        float x = -Input.GetAxisRaw("Mouse Y");
+        transform.Rotate(0, y, 0);
+        camera.transform.Rotate(x, 0, 0);
+        camera.transform.localRotation = ClampRotationAroundXAxis(camera.transform.localRotation);
+    }
+
+    private void HandleMovement() {
+        float forward;
+        float strafe;
+
+        if (Input.GetButton("Sprint")) {
+            hb.speedMult = sprintSpeedMultiplier;
+            forward = Input.GetAxisRaw("Horizontal") * speed * sprintSpeedMultiplier;
+            strafe = Input.GetAxisRaw("Vertical") * speed * sprintSpeedMultiplier;
+        }
+        else {
+            hb.speedMult = 1;
+            forward = Input.GetAxisRaw("Horizontal") * speed;
+            strafe = Input.GetAxisRaw("Vertical") * speed;
+        }
+
+        Vector3 move = new Vector3(forward, rb.velocity.y, strafe);
+        rb.velocity = transform.TransformDirection(move);
+
+        if (Input.GetAxisRaw("Jump") > 0) {
+            if (IsGrounded()) {
+                rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
+            }
+
+        }
+    }
+
+    // Raycast down to see if player is standing on a collider
+    public bool IsGrounded() {
+		if (Physics.Raycast(floorDetector.position, Vector3.down, 0.05f))
+			return true;
+		else
+			return false;
+	}
+
+
+    // Don't ask me how this works it just keeps the camera from rotationg 360 degrees on the x-axis
+    Quaternion ClampRotationAroundXAxis(Quaternion q) {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -90F, 90F);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+}
