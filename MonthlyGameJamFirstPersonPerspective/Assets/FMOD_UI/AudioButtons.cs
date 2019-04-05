5fd4c9b1e6671a3659bb04367c3720a6253400f7
webthingee <sean@webthingee.com>
2019-04-04 18:40:31 -0700

    FMOD_UI for testing and adaptation

---
+++ MonthlyGameJamFirstPersonPerspective/Assets/FMOD_UI/AudioButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioButtons : MonoBehaviour
+{
+    public Button closeButton;
+    
+    private void Start()
+    {
+        closeButton?.GetComponent<Button>().onClick.AddListener(ToggleActive);
+    }
+
+    public void ToggleActive()
+    {
+        GetComponentInParent<AudioMaster>().ToggleActive();
+    }
+}
---
+++ MonthlyGameJamFirstPersonPerspective/Assets/FMOD_UI/AudioSliders.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSliders : MonoBehaviour
+{
+    #pragma warning disable    
+    [Header("Sliders")]
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+    #pragma warning enable
+
+    private AudioMaster audioMaster;
+
+    private void Start()
+    {
+        SetUpAudioMaster();
+        
+        // Set Up Controls
+        masterVolumeSlider.value = AudioMaster.instance.mainVolume;
+        masterVolumeSlider.onValueChanged.AddListener(AudioMaster.instance.SetMasterVolume);
+
+        musicVolumeSlider.value = AudioMaster.instance.musicVolume;
+        musicVolumeSlider.onValueChanged.AddListener(AudioMaster.instance.SetMusicVolume);
+
+        sfxVolumeSlider.value = AudioMaster.instance.sfxVolume;
+        sfxVolumeSlider.onValueChanged.AddListener(AudioMaster.instance.SetSfxVolume);
+    }
+
+    private void SetUpAudioMaster()
+    {
+        if (AudioMaster.instance == null) // might need script order adjustment
+        {
+            Debug.Log("There is no AudioMaster to control");
+        }
+    }
+}
